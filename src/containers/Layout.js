eb36dccc351b7574d4cfd79742ca66eab7c4c8b1
Ricardo <ricardo.sousa13@hotmail.com>
2023-03-04 12:26:52 +0000

    added layout container

---
+++ src/containers/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Layout() {
+  return (
+    <h1 className="text-3xl font-bold underline text-center  ">
+    Hello world, this is thelayout component!
+    </h1>
+  );
+}
+
+export default Layout
